496389ebc4b162f058beff4db798dd6c30997a7a
paulo-leitao <paulovictor.engca@gmail.com>
2022-05-25 00:27:17 -0300

    Refatorando Atividade DOM

--- javascript/DOM/assets/js/scripts.js
+++ javascript/DOM/assets/js/scripts.js
@@ -1,23 +1,27 @@
-let themeBtn = document.getElementById("mode-selector");
+let themeBtn = document.getElementById('mode-selector');
+let body = document.getElementsByTagName('body')[0];
+let footer = document.getElementsByTagName('footer')[0];
+let h1 = document.getElementsByTagName('h1')[0];
+
 
 function changeMode() {
-    let body = document.getElementsByTagName('body')[0];
-    let footer = document.getElementsByTagName('footer')[0];
-    let h1 = document.getElementsByTagName('h1')[0];
-    
-    const darkModeOn = body.classList.contains('dark-mode');
-    
     body.classList.toggle('dark-mode');
     footer.classList.toggle('dark-mode');
     h1.classList.toggle('dark-mode');
-    themeBtn.classList.toggle('dark-mode');
+    themeBtn.classList.toggle('dark-mode');    
+
+    changeText();
+}
+
+function changeText() {
+    h1.textContent = "Light Mode On"
+    themeBtn.textContent = "Dark Mode";
     
-    h1.textContent = "Dark Mode On"
-    themeBtn.textContent = "Light Mode";
+    const darkModeOn = body.classList.contains('dark-mode');
     
     if ( darkModeOn ){
-        h1.textContent = "Light Mode On"
-        themeBtn.textContent = "Dark Mode";
+        h1.textContent = "Dark Mode On"
+        themeBtn.textContent = "Light Mode";
     }
 }
 
